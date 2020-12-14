d123bb629c5f73057111421288e1e4bb1156c2bd
malinleven <malinel2021@student.cis.edu.hk>
2020-12-14 11:25:33 +0800

    first commit

---
+++ BookWorm/Utilities.swift
@@ -0,0 +1,9 @@
+//
+//  Utilities.swift
+//  BookWorm
+//
+//  Created by Malin Leven on 8/12/2020.
+//  Copyright © 2020 Malin Leven. All rights reserved.
+//
+
+import Foundation
---
+++ BookWorm/ViewControllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  BookWorm
+//
+//  Created by Malin Leven on 4/12/2020.
+//  Copyright © 2020 Malin Leven. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
